74186b5e5987395f14eceb080d794b9911356600
Cameron Garcia <cdgarcia20@my.trine.edu>
2022-10-07 15:20:10 -0400

    Updated Keypad Control system 10/7/2022

---
+++ Core/Inc/Keypad.h
@@ -0,0 +1,15 @@
+/*
+ * Keypad.h
+ *
+ *  Created on: Sep 29, 2022
+ *      Author: cdgarcia20
+ */
+
+#ifndef INC_KEYPAD_H_
+#define INC_KEYPAD_H_
+
+
+
+#endif /* INC_KEYPAD_H_ */
+void key_pressed(void);
+char get_keypd_key(void);
